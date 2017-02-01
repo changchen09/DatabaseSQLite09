728b8e626366dc590a8f1b38bdde4f906dba106e
changchen09 <changchen09@gmail.com>
2017-02-01 23:10:42 +0530

    Initial commit

---
+++ app/src/main/java/com/changchen09/databasesqlite/Comment.java
@@ -0,0 +1,33 @@
+package com.changchen09.databasesqlite;
+
+/**
+ * Created by changchen09 on 1/30/2017.
+ */
+
+public class Comment {
+
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
